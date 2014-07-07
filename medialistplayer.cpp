6cf0cdd41c9ecb2005897e1035614a57b294edd3
raf924 <raf924@gmail.com>
2014-07-07 19:34:55 +0200

    Added events to MediaListPlayer

--- medialistplayer.cpp
+++ medialistplayer.cpp
@@ -3,4 +3,35 @@
 MediaListPlayer::MediaListPlayer(QObject *parent) :
     QObject(parent)
 {
+    _mediaListPlayer = libvlc_media_list_player_new(VlcInstance::load()->instance());
+    _events = VlcInstance::load()->mediaListPlayerEvents();
+}
+
+MediaListPlayer::~MediaListPlayer()
+{
+    foreach (const libvlc_event_e & event, _events) {
+        libvlc_event_detach(libvlc_media_list_player_event_manager(_mediaListPlayer),event,processEvent,this);
+    }
+    libvlc_media_list_player_release(_mediaListPlayer);
+}
+
+void MediaListPlayer::processEvent(const libvlc_event_t *event, void *data)
+{
+    MediaListPlayer *self = (MediaListPlayer *)data;
+    qDebug()<<libvlc_event_type_name(event->type);
+    switch(event->type){
+    case libvlc_MediaListPlayerNextItemSet:
+        break;
+    case libvlc_MediaListPlayerPlayed:
+        break;
+    case libvlc_MediaListPlayerStopped:
+        break;
+    }
+}
+
+void MediaListPlayer::registerEvents()
+{
+    foreach (const libvlc_event_e  & event, _events) {
+        libvlc_event_attach(libvlc_media_list_player_event_manager(_mediaListPlayer),event,processEvent,this);
+    }
 }
--- medialistplayer.h
+++ medialistplayer.h
@@ -2,16 +2,23 @@
 #define MEDIALISTPLAYER_H
 
 #include <QObject>
+#include <vlc/vlc.h>
+#include "medialist.h"
 
 class MediaListPlayer : public QObject
 {
     Q_OBJECT
 public:
     explicit MediaListPlayer(QObject *parent = 0);
-
+    ~MediaListPlayer();
 signals:
 
 public slots:
+private:
+    EventList _events;
+    static void processEvent(const libvlc_event_t * event, void *data);
+    void registerEvents();
+    libvlc_media_list_player_t * _mediaListPlayer;
 
 };
 
